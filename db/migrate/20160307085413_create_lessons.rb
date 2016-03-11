2259c010ef52a9ef9ee368b8aafa6255d1081e7e
Nguyen Thanh Luan <nguyen.thanh.luan@framgia.com>
2016-03-11 16:59:57 +0700

    User result

--- db/migrate/20160307085413_create_lessons.rb
+++ db/migrate/20160307085413_create_lessons.rb
@@ -2,6 +2,7 @@ class CreateLessons < ActiveRecord::Migration
   def change
     create_table :lessons do |t|
       t.integer :number_of_right_answer
+      t.integer :status, default: 0
       t.references :user
       t.references :category
 
